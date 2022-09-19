d011a155d4452f796b0b20e6b63b173f7a9a978a
Rizakura0110 <rasakura0110@gmail.com>
2022-09-20 07:24:43 +0900

    eslint導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Date from '../components/date'
 
 export default function Home({
-  allPostsData
 }: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
 }) {
   return (
     <Layout home>
@@ -21,10 +13,10 @@ export default function Home({
       <section className={utilStyles.headingMd}>
         <p>[rizakura]</p>
         <p>
-          <a href="https://twitter.com/rizakura___" target="_blank">Twitter</a>
+          <a href="https://twitter.com/rizakura___" target="_blank" rel="noreferrer">Twitter</a>
         </p>
         <p>
-          <a href="https://github.com/Rizakura0110" target="_blank">GitHub</a>
+          <a href="https://github.com/Rizakura0110" target="_blank" rel="noreferrer">GitHub</a>
         </p>
       </section>
     </Layout>
