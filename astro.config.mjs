72e6acf8aba2565e52df2eafcace2c783e8314e8
rizakura <rasakura0110@gmail.com>
2025-09-17 22:13:42 +0900

    URL修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://rizakura.me',
   vite: {
     plugins: [tailwindcss()]
   }
